8fe91f5558aa37f708311f443ccb91d5acdbbd6d
KristinaBal <105514019+KristinaBal@users.noreply.github.com>
2022-05-13 22:56:04 +0200

    naujas kodas

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
-let obj = {
-    array:[1,2,3,4,5,6],
-    name:"kristina",
-    surname:"baltrunine"
-}
+// let obj = {
+//     array:[1,2,3,4,5,6],
+//     name:"kristina",
+//     surname:"saturnine"
+// }
 
-console.log(obj.array);
-console.log(obj.name);
-console.log(obj.surname);
+// console.log(obj.array);
+// console.log(obj.name);
+// console.log(obj.surname);
+
+const array = [1, 2, 3, 4, 5, 6]
+console.log(array);
+for (let i = 0; i < array.length; i++) {
+    let element = array[i];
+}
